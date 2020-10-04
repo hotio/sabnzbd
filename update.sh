dcb9705a30d1d0340195b6ea54ad60328a0dba88
hotio <26902309+hotio@users.noreply.github.com>
2020-10-04 13:14:25 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -31,5 +31,6 @@ else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sabnzbd/sabnzbd/commits/develop" | jq -r .sha)
     [[ -z ${version} ]] && exit 1
     echo "VERSION=${version}" > VERSION
+    echo '{"version":"'"${version}"'"}' > VERSION.json
     echo "##[set-output name=version;]${version}"
 fi
