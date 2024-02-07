9a69f2bf689dcefbc29d8384b68f0b0295490222
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 09:41:16 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sabnzbd/sabnzbd/releases" | jq -r .[0].tag_name | sed s/v//g)
-[[ -z ${version} ]] && exit 0
-par2turbo_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/animetosho/par2cmdline-turbo/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${par2turbo_version} ]] && exit 0
-nzbnotify_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/nzb-notify/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${nzbnotify_version} ]] && exit 0
-old_version=$(jq -r '.version' < VERSION.json)
-changelog=$(jq -r '.changelog' < VERSION.json)
-[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/sabnzbd/sabnzbd/compare/${old_version}...${version}"
-version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .par2turbo_version = "'"${par2turbo_version}"'" | .nzbnotify_version = "'"${nzbnotify_version}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
+set -e
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sabnzbd/sabnzbd/releases" | jq -re .[0].tag_name)
+par2turbo_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/animetosho/par2cmdline-turbo/releases/latest" | jq -re .tag_name)
+nzbnotify_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/nzb-notify/releases/latest" | jq -re .tag_name)
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    --arg par2turbo_version "${par2turbo_version//v/}" \
+    --arg nzbnotify_version "${nzbnotify_version//v/}" \
+    '.version = $version | .par2turbo_version = $par2turbo_version | .nzbnotify_version = $nzbnotify_version' <<< "${json}" | tee VERSION.json
