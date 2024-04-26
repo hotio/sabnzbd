c7f3980e4ff3b61663a37c215f8be1300a3f90d6
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-04-26 07:10:21 +0200

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest" | jq -re .tag_name) || exit 1
+[[ -z ${version} ]] && exit 0
+[[ ${version} == null ]] && exit 0
 par2turbo_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/animetosho/par2cmdline-turbo/releases/latest" | jq -re .tag_name) || exit 1
+[[ -z ${par2turbo_version} ]] && exit 0
+[[ ${par2turbo_version} == null ]] && exit 0
 nzbnotify_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/nzb-notify/releases/latest" | jq -re .tag_name) || exit 1
+[[ -z ${nzbnotify_version} ]] && exit 0
+[[ ${nzbnotify_version} == null ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
