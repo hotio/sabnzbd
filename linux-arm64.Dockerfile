e9cd4e4561f635892b80c849de7bd757bc5baf9b
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-05-02 06:57:54 +0200

    remove nzbnotify script

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -27,11 +27,6 @@ ENV IMAGE_STATS=${IMAGE_STATS} WEBUI_PORTS="8080/tcp,8080/udp"
 
 COPY --from=builder /usr/local/bin/par2* /usr/local/bin/
 
-ARG NZBNOTIFY_VERSION
-RUN mkdir "${APP_DIR}-scripts" && \
-    curl -fsSL "https://github.com/caronc/nzb-notify/archive/refs/tags/v${NZBNOTIFY_VERSION}.tar.gz" | tar xzf - -C "${APP_DIR}-scripts" --strip-components=1 && \
-    chmod -R u=rwX,go=rX "${APP_DIR}-scripts"
-
 ARG VERSION
 RUN curl -fsSL "https://github.com/sabnzbd/sabnzbd/archive/${VERSION}.tar.gz" | tar xzf - -C "${APP_DIR}" --strip-components=1 && \
     cd "${APP_DIR}" && python3 tools/make_mo.py && \
@@ -48,7 +43,6 @@ RUN apk add --no-cache py3-pip && \
         rust && \
     pip3 install --break-system-packages --upgrade pip && \
     pip3 install --break-system-packages -r "${APP_DIR}/requirements.txt" && \
-    pip3 install --break-system-packages -r "${APP_DIR}-scripts/requirements.txt" && \
     apk del --purge build-dependencies
 
 COPY root/ /
--- update-versions.sh
+++ update-versions.sh
@@ -5,12 +5,8 @@ version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.co
 par2turbo_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/animetosho/par2cmdline-turbo/releases/latest" | jq -re .tag_name) || exit 1
 [[ -z ${par2turbo_version} ]] && exit 0
 [[ ${par2turbo_version} == null ]] && exit 0
-nzbnotify_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/caronc/nzb-notify/releases/latest" | jq -re .tag_name) || exit 1
-[[ -z ${nzbnotify_version} ]] && exit 0
-[[ ${nzbnotify_version} == null ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
     --arg par2turbo_version "${par2turbo_version//v/}" \
-    --arg nzbnotify_version "${nzbnotify_version//v/}" \
-    '.version = $version | .par2turbo_version = $par2turbo_version | .nzbnotify_version = $nzbnotify_version' <<< "${json}" | tee VERSION.json
+    '.version = $version | .par2turbo_version = $par2turbo_version' <<< "${json}" | tee VERSION.json
