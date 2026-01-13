449cac720c5c9aebb42f4314b7b72d467ee6048b
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-13 10:12:58 +0100

    rename args [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -2,9 +2,9 @@
 set -exuo pipefail
 
 version=$(curl -fsSL "https://api.github.com/repos/sabnzbd/sabnzbd/commits/develop" | jq -re .sha)
-par2turbo_version=$(curl -fsSL "https://api.github.com/repos/animetosho/par2cmdline-turbo/releases/latest" | jq -re .tag_name)
+version_par2turbo=$(curl -fsSL "https://api.github.com/repos/animetosho/par2cmdline-turbo/releases/latest" | jq -re .tag_name)
 json=$(cat meta.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
-    --arg par2turbo_version "${par2turbo_version//v/}" \
-    '.version = $version | .par2turbo_version = $par2turbo_version' <<< "${json}" | tee meta.json
+    --arg version_par2turbo "${version_par2turbo//v/}" \
+    '.version = $version | .version_par2turbo = $version_par2turbo' <<< "${json}" | tee meta.json
